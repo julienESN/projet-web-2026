4f2e8071b6ebfe4bb82f1791ddfd8ce7a6cb8063
Sofiane Fares <79220488+FaresSofiane@users.noreply.github.com>
2026-01-20 12:06:54 +0100

    feat: add Users, Resources, Tags, and Categories modules with controllers and services

--- package/backend/src/app.module.ts
+++ package/backend/src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { ResourcesModule } from './resources/resources.module';
+import { TagsModule } from './tags/tags.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, UsersModule, ResourcesModule, TagsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
